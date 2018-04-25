ffb92ba8928171532d012ff2508182fd33a213c4
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-4.local>
2018-04-25 11:46:37 -0700

    fixing naming

--- tests/scale/test_load.py
+++ tests/scale/test_load.py
@@ -8,15 +8,15 @@ import time
 
 @pytest.mark.scale
 def test_create_10_jobs():
-	for x in (0, 9):
-		#create 10 Hello world jobs executing every minute
-		test_job_name = 'test-job-{}'.format(uuid.uuid4())
-		jenkins.create_job(config.SERVICE_NAME, test_job_name)
-		time.pause(0.1)
+    for x in (0, 9):
+        #create 10 Hello world jobs executing every minute
+    	test_job_name = 'test-job-{}'.format(uuid.uuid4())
+    	jenkins.create_job(config.SERVICE_NAME, test_job_name)
+    	time.pause(0.1)
 
 @pytest.mark.scale
 def test_create_10_jobs_custom_command():
-	for x in (0, 99):
+	for x in (0, 9):
 		#create 10 jobs with custom command executing every minute
 		test_job_name = 'test-job-{}'.format(uuid.uuid4())
 		jenkins.create_job(config.SERVICE_NAME, test_job_name, "echo \"test command\";")
@@ -25,7 +25,7 @@ def test_create_10_jobs_custom_command():
 
 @pytest.mark.scale
 def test_create_10_jobs_custom_schedule():
-	for x in (0, 99):
+	for x in (0, 9):
 		#create 10 jobs with custom command executing every 5 minute
 		test_job_name = 'test-job-{}'.format(uuid.uuid4())
 		jenkins.create_job(config.SERVICE_NAME, test_job_name, schedule_frequency_in_min=5)
