5fa8274f75b9498dedbe39eb994f5d0edd9a02f1
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-4.local>
2018-04-25 12:47:59 -0700

    fixing scale tests

--- tests/scale/test_load.py
+++ tests/scale/test_load.py
@@ -8,11 +8,10 @@ import time
 
 @pytest.mark.scale
 def test_create_10_jobs():
-    for x in (0, 9):
+    for x in range(0, 9):
         #create 10 Hello world jobs executing every minute
         test_job_name = 'test-job-{}'.format(uuid.uuid4())
         jenkins.create_job(config.SERVICE_NAME, test_job_name)
-        time.pause(0.1)
 
 @pytest.mark.scale
 def test_create_10_jobs_custom_command():
@@ -20,24 +19,21 @@ def test_create_10_jobs_custom_command():
         #create 10 jobs with custom command executing every minute
         test_job_name = 'test-job-{}'.format(uuid.uuid4())
         jenkins.create_job(config.SERVICE_NAME, test_job_name, "echo \"test command\";")
-        time.pause(0.1)
 
 
 @pytest.mark.scale
 def test_create_10_jobs_custom_schedule():
-    for x in (0, 9):
+    for x in range(0, 9):
         #create 10 jobs with custom command executing every 5 minute
         test_job_name = 'test-job-{}'.format(uuid.uuid4())
         jenkins.create_job(config.SERVICE_NAME, test_job_name, schedule_frequency_in_min=5)
-        time.pause(0.1)
 
 @pytest.mark.scale
 def test_create_100_jobs():
-    for x in (0, 99):
+    for x in range(0, 99):
         #create 100 Helloworld jobs
         test_job_name = 'test-job-{}'.format(uuid.uuid4())
         jenkins.create_job(config.SERVICE_NAME, test_job_name)
-        time.pause(0.1)
 
 
 
